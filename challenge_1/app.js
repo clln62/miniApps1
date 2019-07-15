00f9dc28b40ef0849132f5c2ee67fbc903763b6b
Collin Fairlie <collin.fairlie@gmail.com>
2019-07-15 13:28:46 -0500

    Got each of my squares to display an 'X' without changing size. Now onto saving my count to alternate between 'X' and 'O'.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,21 +1,21 @@
 // create an on click feature that will work for each square
 
 
-function clicked () {
+function clicked (id) {
     let counter = 1;
+    let cell = document.getElementById(id);
     // if counter is odd 
     if (counter % 2 !== 0) {
         // display X
-        document.getElementById("cell").innerHTML = 'X';
+        cell.innerHTML = 'X';
         // increment counter
         counter++;
     } else {
     // else
         // display O
-        document.getElementById("cell").innerHTML = 'O';
+        cell.innerHTML = 'O';
         // increment counter
         counter++;
-
     }
 
 
