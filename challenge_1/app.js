e0ea437634d4b6e31c9682da780276d804732f28
Collin Fairlie <collin.fairlie@gmail.com>
2019-07-15 15:33:47 -0500

    Got the multiple clicks to end, but need to refactor code to recognize information inside of the cell.:

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,22 +1,34 @@
 // create an on click feature that will work for each square
 
+let counter = 1;
+// let storage = {};
 
 function clicked (id) {
-    let counter = 1;
     let cell = document.getElementById(id);
-    // if counter is odd 
-    if (counter % 2 !== 0) {
-        // display X
-        cell.innerHTML = 'X';
-        // increment counter
-        counter++;
-    } else {
-    // else
-        // display O
-        cell.innerHTML = 'O';
-        // increment counter
-        counter++;
-    }
+    let wasClicked = 'clicked';
+    // if (storage[id] !== wasClicked) {
+        // if counter is odd 
+        if (cell.style.color == 'red') {
+            if (counter % 2 !== 0) {
+                // display X
+                cell.innerHTML = 'X';
+                // increment counter
+                counter++;
+                cell.style.display = 'blocked';
+            } else {
+            // else
+                // display O
+                cell.innerHTML = 'O';
+                // increment counter
+                counter++;
+            }
+            cell.style.color = 'black';
+        }
+        // storage.id = wasClicked;
+        // console.log(cell.wasClicked)
+    // }
+}
 
-
+function newGame () {
+    document.getElementById("table").innerHTML = '';
 }
