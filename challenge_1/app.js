9a9b1b6785f34bcca0e229f0c25f867321b2eb17
Collin Fairlie <collin.fairlie@gmail.com>
2019-07-15 13:04:53 -0500

    Got my event onclick to work with displaying an 'X', but only in top, left square, no matter where I click.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,6 +1,22 @@
 // create an on click feature that will work for each square
 
 
-function clicked (event) {
-    console.log('hello')
+function clicked () {
+    let counter = 1;
+    // if counter is odd 
+    if (counter % 2 !== 0) {
+        // display X
+        document.getElementById("cell").innerHTML = 'X';
+        // increment counter
+        counter++;
+    } else {
+    // else
+        // display O
+        document.getElementById("cell").innerHTML = 'O';
+        // increment counter
+        counter++;
+
+    }
+
+
 }
