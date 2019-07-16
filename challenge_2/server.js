5045ab2ea60fda7fa63bfc0e4494c5bd26523b74
Collin Fairlie <collin.fairlie@gmail.com>
2019-07-16 15:41:32 -0500

    Making small changes in order to connect the form in the html doc to the server. Still reading through the LEARN app and information online to get a better understanding of express.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,14 +1,24 @@
-let express = require('express');
+const express = require('express');
+const path = require('path');
 
 let app = express();
 
+app.get('/', (req, res) => {
+    res.json();
+})
+
 app.use(express.static('client'))
 
+const PORT = process.env.PORT || 5000;
 
-app.route('/')
-    .get('/', (req, res) => {
-    res.send('Hello World!')
-})
-    .post('/', (req, res) => {
+// app.get('/', (req, res) => {
+//     res.send('<h1>Hello World!!</h1>')
+// });
+
+app.post('/', (req, res) => {
     res.send('Got a POST request.')
-})
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+}); 
