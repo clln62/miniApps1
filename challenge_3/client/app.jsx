f0a4627f14c4a93aede5d320ae9b0a118057a40e
Collin Fairlie <collin.fairlie@gmail.com>
2019-07-17 11:18:06 -0500

    Got the standard set-up done with my HTML files. Now on to linking everything through the server and app.

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,12 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
 
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+}
 
