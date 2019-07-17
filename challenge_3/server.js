5e794f871565753f2a57018af98cf0e3fb10ed6e
Collin Fairlie <collin.fairlie@gmail.com>
2019-07-17 15:07:59 -0500

    Working through connecting the server to the DB and getting the click functions to work.

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
+
+
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password"
+});
+
+con.connect(function(err) {
+  if (err) {
+      console.error(err);
+      return;
+  };
+  console.log("Connected!");
+  
+});
 
 
 app.use(express.static('public'));
